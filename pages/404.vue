904b07d0a099b58a9368fd18c3371d4695221af4
NikkivB <nikkivanbraam@hotmail.nl>
2022-06-23 10:04:26 +0200

    404 page is now responsive

--- pages/404.vue
+++ pages/404.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="">
     <div class="flex h-screen font-encode -ml-4 -mt-4">
-      <div class="flex self-center w-11/12 mx-auto h-5/6 border-black border-4" id="offset-border">
+      <div class="flex self-center w-3/4 lg:w-11/12 mx-auto md:h-3/5 h-3/6 lg:h-5/6 border-black border-4"
+           id="offset-border">
         <div class=" w-3/6 self-center mx-auto">
-          <p class="text-9xl text-center text-ruddy-pink font-semibold">4 0 4</p>
-          <hr class="mx-4 my-8 border-black border-2">
-          <p class="text-center text-4xl w-11/12 ">Sorry, the site you are trying to connect to is currently under
+          <p class="text-5xl lg:text-9xl text-center text-ruddy-pink font-semibold">4 0 4</p>
+          <hr class="mx-4 my-8 md:my-2 lg-my-8 border-black border-2">
+          <p class="text-center text-lg lg:text-4xl w-11/12 ">Sorry, the site you are trying to connect to is currently
+            under
             construction</p>
         </div>
       </div>
